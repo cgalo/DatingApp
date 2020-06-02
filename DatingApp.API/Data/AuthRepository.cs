f2cfaa70fbad77defe63acb02b4e216c05eeb455
cgalo <cgalo13@gmail.com>
2020-06-02 13:33:18 -0400

    Completed registering the user method

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,53 @@
+/**
+ * This repository will query our database through our Identity Framework
+ * Inject the data content as well
+*/
+
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _content;
+        public AuthRepository(DataContext content)
+        {
+            _content = content; 
+        }
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _content.Users.AddAsync(user);
+            await _content.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;            //Save the salt key value for this password
+                //Now compute the hash
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }   // End of interface
+}   // End of namespace
